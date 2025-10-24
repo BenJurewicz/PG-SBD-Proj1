12acac8d27fcb5b02f005351a9ff78976366b064
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-10-24 14:28:15 +0200

    Init Commit

---
+++ src/create_files.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from create_files!" << std::endl;
+    return 0;
+}
---
+++ src/sort_files.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from sort_files!" << std::endl;
+    return 0;
+}
