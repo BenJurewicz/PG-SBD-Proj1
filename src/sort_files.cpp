522af2cff3716fbed2113972c5c2fccb27e1e524
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-18 11:53:48 +0100

    wip

--- src/sort_files.cpp
+++ src/sort_files.cpp
@@ -1,4 +1,3 @@
-#include <cstdint>
 #include <file_buffering.hpp>
 #include <iostream>
 #include <string>
@@ -10,6 +9,15 @@ constexpr unsigned int bufferCount = 5;
 int main() {
     BufferedFile f("temp/testFile.txt");
 
+    for (int i = 0; i < 20; i++) {
+        f.write(i, std::to_string(i));
+    }
+
+    f.write(13, "123456789012345678901234567890");
+    f.write(33, "123456789012345678901234567890");
+
+    std::cout << "Hello from sort_files!" << std::endl;
+
     std::vector<std::vector<Record>> buffers(bufferCount);
     // TODO: Fill each buffer with a single page from the BufferedFile
     // TODO: Figure out how to sort buffers
@@ -17,7 +25,8 @@ int main() {
     // or std::sort(buffers | std::views::join) in c++20
     // TODO: Write the buffers back to the file
     // NOTE: BufferedFile.writePage and BufferedFile.readPage would probabbly be
-    // usefull NOTE: Change BufferedFile so that reads returns a std::optional
+    // usefull
+    // NOTE: Change BufferedFile so that reads returns a std::optional
     // so it can fail if you leave the file and write will write anywhere in
     // the already written or directly after it
     return 0;
