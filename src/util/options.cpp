d3da80fa1cedb571d20a7355853e8a6705dfc884
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-20 01:45:10 +0100

    Comment out the options

--- src/util/options.cpp
+++ src/util/options.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <mutex>
-#include <options.hpp>
-
-Options::Options(int argc, char* argv[]) {
-    std::call_once(init_flag, [&]() { parseOptions(argc, argv); });
-}
-
-void Options::parseOptions(int argc, char* argv[]) {
-    reportIncorret(argc, argv);
-    parseBufferCount(argc, argv);
-    parseRecordsPerPage(argc, argv);
-    parseHelp(argc, argv);
-}
-
-void Options::parseHelp(int argc, char* argv[]) {}
-
-void Options::printHelp() {
-    std::cout << "Options:\n"
-                 "  -n=[n]  n>2 buffers (required)\n"
-                 "  -b=[b]  b>0 blocking factor (required) \n"
-                 "  -p      disable printing after every phase (optional)\n";
-}
-
-void Options::parseBufferCount(int argc, char* argv[]) {}
-void Options::parseRecordsPerPage(int argc, char* argv[]) {}
-void Options::parsePrinting(int argc, char* argv[]) {}
+// #include <iostream>
+// #include <mutex>
+// #include <options.hpp>
+//
+// Options::Options(int argc, char* argv[]) {
+//     std::call_once(init_flag, [&]() { parseOptions(argc, argv); });
+// }
+//
+// void Options::parseOptions(int argc, char* argv[]) {
+//     reportIncorret(argc, argv);
+//     parseBufferCount(argc, argv);
+//     parseRecordsPerPage(argc, argv);
+//     parseHelp(argc, argv);
+// }
+//
+// void Options::parseHelp(int argc, char* argv[]) {}
+//
+// void Options::printHelp() {
+//     std::cout << "Options:\n"
+//                  "  -n=[n]  n>2 buffers (required)\n"
+//                  "  -b=[b]  b>0 blocking factor (required) \n"
+//                  "  -p      disable printing after every phase (optional)\n";
+// }
+//
+// void Options::parseBufferCount(int argc, char* argv[]) {}
+// void Options::parseRecordsPerPage(int argc, char* argv[]) {}
+// void Options::parsePrinting(int argc, char* argv[]) {}
--- src/util/options.hpp
+++ src/util/options.hpp
@@ -1,26 +1,26 @@
-#ifndef OPTIONS_HPP
-#define OPTIONS_HPP
-
-#include <cstddef>
-#include <mutex>
-class Options {
-   public:
-    Options(int argc, char* argv[]);
-
-   private:
-    size_t bufferCount = 5;
-    size_t recordsPerPage = 20;  // Blocking factor
-    bool printing = true;
-    std::once_flag init_flag;
-
-    void parseOptions(int argc, char* argv[]);
-
-    void reportIncorret(int argc, char* argv[]);
-    void parseBufferCount(int argc, char* argv[]);
-    void parseRecordsPerPage(int argc, char* argv[]);
-    void parsePrinting(int argc, char* argv[]);
-    void parseHelp(int argc, char* argv[]);
-    void printHelp();
-};
-
-#endif  // !OPTIONS_HPP
+// #ifndef OPTIONS_HPP
+// #define OPTIONS_HPP
+//
+// #include <cstddef>
+// #include <mutex>
+// class Options {
+//    public:
+//     Options(int argc, char* argv[]);
+//
+//    private:
+//     size_t bufferCount = 5;
+//     size_t recordsPerPage = 20;  // Blocking factor
+//     bool printing = true;
+//     std::once_flag init_flag;
+//
+//     void parseOptions(int argc, char* argv[]);
+//
+//     void reportIncorret(int argc, char* argv[]);
+//     void parseBufferCount(int argc, char* argv[]);
+//     void parseRecordsPerPage(int argc, char* argv[]);
+//     void parsePrinting(int argc, char* argv[]);
+//     void parseHelp(int argc, char* argv[]);
+//     void printHelp();
+// };
+//
+// #endif  // !OPTIONS_HPP
