ba68a62ecb87171b2ff525bf4b37678835bdab5c
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-23 19:17:56 +0100

    Extract merging to a separate function

--- src/sort_files.cpp
+++ src/sort_files.cpp
@@ -12,6 +12,7 @@
 #include "util/sort_options.hpp"
 
 void createRunsInFile(BufferedFile& f, const SortOptions& options);
+void mergeRuns(BufferedFile& f, const SortOptions& options);
 
 int main(int argc, char** argv) {
     SortOptions options(argc, argv);
@@ -22,14 +23,94 @@ int main(int argc, char** argv) {
     f.printFileContent();
     std::cout << std::endl;
 
-    // size_t maxi = 100;
-    // for (size_t i = 0; i < maxi; i++) {
-    //     f.write(i, std::to_string(maxi - 1 - i));
-    // }
-    // f.flush();
-
     createRunsInFile(f, options);
 
+    mergeRuns(f, options);
+
+    std::cout << "\nFinished" << std::endl;
+    std::cout << "Write Count: " << BufferedFile::writeCount << std::endl;
+    std::cout << "Read Count: " << BufferedFile::readCout << std::endl;
+    std::cout << "Sorted contents: " << std::endl;
+    f.printFileContent();
+    return 0;
+}
+
+void createRunsInFile(BufferedFile& f, const SortOptions& options) {
+    if (options.isLogging()) {
+        std::cout << "Stage 1: Divide into runs" << std::endl;
+    }
+
+    std::vector<std::vector<Record>> buffers(options.getBufferCount());
+
+    auto cmp = [&](auto& a, auto& b) {
+        return buffers[a.first][a.second] > buffers[b.first][b.second];
+    };
+    std::priority_queue<
+        std::pair<size_t, size_t>,
+        std::vector<std::pair<size_t, size_t>>,
+        decltype(cmp)>
+        pq(cmp);
+
+    auto [fBegin, fEnd] = f.pages();
+    bool isFileEmpty = false;
+
+    Buffer outBuf;
+    outBuf = f.pages();
+
+    size_t runCount = 0;
+
+    while (!isFileEmpty) {
+        // NOTE: Fill all buffers
+        for (auto& b : buffers) {
+            if (fBegin == fEnd) {
+                isFileEmpty = true;
+                break;
+            }
+            b = *fBegin++;
+        }
+        if (std::ranges::all_of(buffers, [](auto& b) { return b.empty(); })) {
+            break;
+        }
+
+        // NOTE: Sort:
+        for (auto& b : buffers) {
+            std::ranges::sort(b);
+        }
+
+        // NOTE: Initialize pg with first element from each nonempty buffer
+        for (size_t i = 0; i < buffers.size(); i++) {
+            if (!buffers[i].empty()) {
+                pq.push({i, 0});
+            }
+        }
+
+        // NOTE: K-way merge
+        while (!pq.empty()) {
+            auto [bufIdx, elemIdx] = pq.top();
+            pq.pop();
+
+            outBuf.append(buffers[bufIdx][elemIdx]);
+
+            // Add next element from same buffer
+            if (elemIdx + 1 < buffers[bufIdx].size()) {
+                pq.push({bufIdx, elemIdx + 1});
+            }
+        }
+
+        for (auto& b : buffers) {
+            b.clear();
+        }
+
+        if (options.isLogging()) {
+            std::cout << "Run " << ++runCount << ":" << std::endl;
+            std::cout << "File contents:" << std::endl;
+            f.printFileContent();
+            std::cout << std::endl;
+        }
+    }
+}
+
+void mergeRuns(BufferedFile& f, const SortOptions& options) {
     if (options.isLogging()) {
         std::cout << "Stage 2: Merging runs\n" << std::endl;
     }
@@ -137,87 +218,5 @@ int main(int argc, char** argv) {
         BufferedFile* temp = dest;
         dest = src;
         src = temp;
-    }
-
-    std::cout << "\nFinished" << std::endl;
-    std::cout << "Write Count: " << BufferedFile::writeCount << std::endl;
-    std::cout << "Read Count: " << BufferedFile::readCout << std::endl;
-    std::cout << "Sorted contents: " << std::endl;
-    f.printFileContent();
-    return 0;
-}
-
-void createRunsInFile(BufferedFile& f, const SortOptions& options) {
-    if (options.isLogging()) {
-        std::cout << "Stage 1: Divide into runs" << std::endl;
-    }
-
-    std::vector<std::vector<Record>> buffers(options.getBufferCount());
-
-    auto cmp = [&](auto& a, auto& b) {
-        return buffers[a.first][a.second] > buffers[b.first][b.second];
-    };
-    std::priority_queue<
-        std::pair<size_t, size_t>,
-        std::vector<std::pair<size_t, size_t>>,
-        decltype(cmp)>
-        pq(cmp);
-
-    auto [fBegin, fEnd] = f.pages();
-    bool isFileEmpty = false;
-
-    Buffer outBuf;
-    outBuf = f.pages();
-
-    size_t runCount = 0;
-
-    while (!isFileEmpty) {
-        // NOTE: Fill all buffers
-        for (auto& b : buffers) {
-            if (fBegin == fEnd) {
-                isFileEmpty = true;
-                break;
-            }
-            b = *fBegin++;
-        }
-        if (std::ranges::all_of(buffers, [](auto& b) { return b.empty(); })) {
-            break;
-        }
-
-        // NOTE: Sort:
-        for (auto& b : buffers) {
-            std::ranges::sort(b);
-        }
-
-        // NOTE: Initialize pg with first element from each nonempty buffer
-        for (size_t i = 0; i < buffers.size(); i++) {
-            if (!buffers[i].empty()) {
-                pq.push({i, 0});
-            }
-        }
-
-        // NOTE: K-way merge
-        while (!pq.empty()) {
-            auto [bufIdx, elemIdx] = pq.top();
-            pq.pop();
-
-            outBuf.append(buffers[bufIdx][elemIdx]);
-
-            // Add next element from same buffer
-            if (elemIdx + 1 < buffers[bufIdx].size()) {
-                pq.push({bufIdx, elemIdx + 1});
-            }
-        }
-
-        for (auto& b : buffers) {
-            b.clear();
-        }
-
-        if (options.isLogging()) {
-            std::cout << "Run " << ++runCount << ":" << std::endl;
-            std::cout << "File contents:" << std::endl;
-            f.printFileContent();
-            std::cout << std::endl;
-        }
     }
 }
