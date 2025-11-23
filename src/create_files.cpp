390113d70acfc8fa52dd287ed480f28eed13ae0b
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-23 15:04:52 +0100

    The Big Rename (in create_files.cpp)

--- src/create_files.cpp
+++ src/create_files.cpp
@@ -9,27 +9,26 @@
 
 std::default_random_engine rng{std::random_device{}()};
 
-std::string generate_random_string(int maxLength, bool numOnly = false) {
-    std::uniform_int_distribution<int> len_dist(1, maxLength);
+std::string generateRandomString(int maxLength, bool numOnly = false) {
+    std::uniform_int_distribution<int> lenDist(1, maxLength);
     // ASCII printable characters are in rage [32,126]
     // but whe start from 33 as (char)32 is space.
     // NOTE: (char)33 == '!'; (char)126 == '~';
-    // TODO: Ocasinally got non ascii chars here
-    std::uniform_int_distribution<int> all_char_dist(33, 126);
-    std::uniform_int_distribution<int> num_dist('0', '9');
+    std::uniform_int_distribution<int> allCharDist(33, 126);
+    std::uniform_int_distribution<int> numDist('0', '9');
 
-    auto char_dist = numOnly ? num_dist : all_char_dist;
+    auto charDist = numOnly ? numDist : allCharDist;
 
-    int length = len_dist(rng);
+    int length = lenDist(rng);
     std::string random_string;
     random_string.reserve(length);
     for (int i = 0; i < length; ++i) {
-        random_string += static_cast<char>(char_dist(rng));
+        random_string += static_cast<char>(charDist(rng));
     }
     return random_string;
 }
 
-void manual_mode(std::vector<std::string>& lines) {
+void manualMode(std::vector<std::string>& lines) {
     std::string line;
     // clang-format off
     std::cout << 
@@ -63,26 +62,26 @@ void manual_mode(std::vector<std::string>& lines) {
     }
 }
 
-void auto_mode(
-    std::vector<std::string>& lines, size_t num_strings, bool numOnly = false
+void autoMode(
+    std::vector<std::string>& lines, size_t numStrings, bool numOnly = false
 ) {
-    for (size_t i = 0; i < num_strings; ++i) {
-        lines.push_back(generate_random_string(Record::maxLen, numOnly));
+    for (size_t i = 0; i < numStrings; ++i) {
+        lines.push_back(generateRandomString(Record::maxLen, numOnly));
     }
-    std::cout << num_strings << " random strings generated." << std::endl;
+    std::cout << numStrings << " random strings generated." << std::endl;
 }
 
-void write_line(std::ofstream& file, const std::string& data) {
+void writeLine(std::ofstream& file, const std::string& data) {
     std::string tempStr = data;
     tempStr.resize(Record::maxLen, '\0');
     file.write(tempStr.data(), Record::maxLen);
 }
 
-void save_file(std::vector<std::string>& lines, const std::string& filename) {
+void saveFile(std::vector<std::string>& lines, const std::string& filename) {
     std::ofstream outfile(filename, std::ios::out | std::ios::binary);
     if (outfile.is_open()) {
         for (const auto& line : lines) {
-            write_line(outfile, line);
+            writeLine(outfile, line);
         }
         std::cout << "Data saved to " << filename << std::endl;
     } else {
@@ -95,13 +94,13 @@ int main(int argc, char** argv) {
     std::vector<std::string> lines;
 
     if (options.isInteractiveMode()) {
-        manual_mode(lines);
+        manualMode(lines);
     } else if (options.isRandomMode()) {
-        auto_mode(lines, options.getRandomCount(), options.isNumbersOnly());
+        autoMode(lines, options.getRandomCount(), options.isNumbersOnly());
     }
 
     if (!lines.empty()) {
-        save_file(lines, options.getFileName());
+        saveFile(lines, options.getFileName());
     }
 
     return 0;
